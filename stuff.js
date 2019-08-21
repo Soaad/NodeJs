85efbb61cabd8dc8495415f2aa37c8212d50974e
Soaad <Soaad@users.noreply.github.com>
2019-08-21 23:10:08 +0200

    making export multiple fns a little bit easiear

--- stuff.js
+++ stuff.js
@@ -1,12 +1,8 @@
-var counter=function(arr){
+module.exports.counter=function(arr){
     return 'there are '+arr.length+' elements';
-}
-var adder=function(a,b){
+};
+module.exports.adder=function(a,b){
     return `the sum of 2 number is ${a+b}`
-}
-var pi=3.14
+};
+module.exports.pi=3.14;
 
-module.exports.counter=counter;
-
-module.exports.adder=adder;
-module.exports.pi=pi;
