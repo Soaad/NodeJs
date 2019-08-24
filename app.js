11cda760b57e4527025d5f34bbe298569c3334e5
Soaad <Soaad@users.noreply.github.com>
2019-08-24 18:08:27 +0200

    apply inherit event emitters

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 
-
-var events=require('events');
 //basic example
+/*var events=require('events');
+
 var myEmitter= new events.EventEmitter
 
 myEmitter.on('someEvent',function(msg){
     console.log(msg);
 });
-myEmitter.emit('someEvent','the event was emitted');
+myEmitter.emit('someEvent','the event was emitted');*/
+
+
+var events=require('events');
+var util=require('util');
+
+var Person=function(name){
+    this.name=name;
+}
+util.inherits(Person,events.EventEmitter);
+
+var Soaad=new Person('Soaad');
+var Ali=new Person('Ali');
+var Hamza=new Person('Hamza');
+
+var people=[Soaad,Ali,Hamza];
+people.forEach(function(person)
+{
+    person.on('speak',function(msg){
+        console.log(`Hi this ${person.name} who said ${msg}`);
+    })
+});
+
+Soaad.emit('speak','Hey dudes');
--- stuff.js
+++
@@ -1,8 +0,0 @@
-module.exports.counter=function(arr){
-    return 'there are '+arr.length+' elements';
-};
-module.exports.adder=function(a,b){
-    return `the sum of 2 number is ${a+b}`
-};
-module.exports.pi=3.14;
-
